efd202c1fc53690370d9ca1a13ce547747d7be0e
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2022-02-01 19:14:53 -0500

    new

--- sql-ddl-design/music.sql
+++ sql-ddl-design/music.sql
@@ -31,3 +31,5 @@ VALUES
   ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
   ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
   ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+
+
